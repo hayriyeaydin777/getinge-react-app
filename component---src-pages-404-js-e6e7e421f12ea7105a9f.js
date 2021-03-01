7949ef07ee848b9dc438cfe534b3e69f1bed6bff
hayriyeaydin777 <hayriyeaydin777@users.noreply.github.com>
2021-03-01 06:27:01 +0000

    deployed via Gatsby Publish Action 🎩 for b165a29df3d3b7553c64e79613ca94f3856b3808

---
+++ component---src-pages-404-js-e6e7e421f12ea7105a9f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("rGDf"),r=a("MAJE"),l=a("v7Hm"),o=a("q1tI"),u=a.n(o),c=a("vrFN");t.default=function(){return u.a.createElement(u.a.Fragment,null,u.a.createElement(c.a,{title:"404: Not found"}),u.a.createElement(n.a,null,"404: Not Found"),u.a.createElement(r.a,{fontSize:"xl",my:5},"You just hit a route that doesn't exist... the sadness."),u.a.createElement(l.a,{bg:"brandPurple"},"Welcome"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e6e7e421f12ea7105a9f.js.map
